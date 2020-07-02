2dfdb25787730282e51442239ee55ab8b8980386
wattrg <robertwhat@hotmail.com>
2020-07-02 11:00:23 +1000

    bash script to compile code

---
+++ exec/make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ../lua-5.3.5/src
+make linux
+cd ../../exec
+
+
+
+g++ ../src/main.cpp                                          \
+../src/particle.cpp                                          \
+../src/advance.cpp                                           \
+../src/mesh.cpp                                              \
+../src/EM.cpp                                                 \
+-o PIC.ex                                                    \
+-I ../lua-5.3.5/src/                                         \
+-I ../LuaBridge/Source/LuaBridge/                            \
+-I ../LuaBridge/Source/                                      \
+-I ../LuaBridge/Source                                       \
+-L ../lua-5.3.5/src/ -llua -ldl -v
